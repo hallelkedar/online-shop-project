5e2988de87ad368401dbe8031e425cda334d3d08
Hallel <hallelkedar000@gmail.com>
2026-07-09 16:28:58 +0300

    Added middleware for json() answars, and get checks

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express";
 import "dotenv/config";
-import accountRouter from "../routes/accountRouter.js";
-import cartRouter from "../routes/cartRouter.js";
-import ordersRouter from "../routes/ordersRouter.js";
-import productRouter from "../routes/productsRouter.js";
+import accountRouter from "./routes/accountRouter.js";
+import cartRouter from "./routes/cartRouter.js";
+import ordersRouter from "./routes/ordersRouter.js";
+import productRouter from "./routes/productsRouter.js";
 
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use(express.json())
 
 app.use("/account", accountRouter);
 app.use("/cart", cartRouter);
@@ -15,11 +17,11 @@ app.use("/orders", ordersRouter);
 app.use("/products", productRouter);
 
 app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to shoes shop server api" });
+  res.json({ success: true, msg: "Welcome to shoes shop server api" });
 });
 
-app.get("/health", (res, req) => {
-  res.json({ msg: "server is up!" });
+app.get("/health", (req, res) => {
+  res.json({ success: true, msg: "server is up!" });
 });
 
 app.listen(PORT, () => {
